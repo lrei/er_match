4e5163e76ff780b688a2710f3bda4cf9679a5f74
Luis Rei <me@luisrei.com>
2015-01-22 19:56:55 +0100

    kyoto cabinet

--- erservice.py
+++ erservice.py
@@ -1,10 +1,10 @@
-import rocksdb
+import kyotocabinet as kc
 from datetime import date, timedelta
 from tweet_common import tweet_archive_min_date
 from er_common import er_get_urls
 
 
-DBFILENAME = 'er.db'
+DBFILENAME = 'er.db.kch'
 ARCHIVEDIR = '../tweets'
 FETCH_DAYS_BEFORE = 1
 
@@ -13,27 +13,23 @@ def db_open(dbfile=DBFILENAME, create=False, bulk=False):
     '''Opens the databe. If create=False, throws exeption if it does not exist
     '''
 
-    opts = rocksdb.Options()
-    opts.create_if_missing = create
-    opts.max_open_files = 300000
-    opts.write_buffer_size = 67108864  # 64MB
-    opts.max_write_buffer_number = 2
-    opts.target_file_size_base = 67108864  # 64MB
-    #opts.block_cache = rocksdb.LRUCache(1 * (1024 ** 3))  # 1GB
-    #opts.block_cache_compressed = rocksdb.LRUCache(256 * (1024 ** 2))  # 256MB
-    opts.disable_data_sync = bulk
+    db = kc.DB()
+    if create:
+        if not db.open(dbfile, kc.DB.OWRITER | kc.DB.OCREATE):
+            print("Failed to create")
+            return None
+    elif not db.open(dbfile, kc.DB.OWRITER):
+        print("Failed to open")
+        return None
 
-    db = rocksdb.DB(dbfile, opts)
     return db
 
 
 def add_urlmap(db, urlmap):
     '''Adds a URL -> EventID dictionary to the DB
     '''
-    batch = rocksdb.WriteBatch()
     for key, value in urlmap.items():
-        batch.put(key, value)
-    db.write(batch)
+        db.add(key, value)
 
 
 def db_init():
@@ -45,25 +41,39 @@ def db_init():
     date_fetch_start = date_min - batch_interval
     date_fetch_end = date_min
 
-    while date_fetch_start < date_max:
-        print('fetching between %s and %s' % (date_fetch_start.str(),
-                                              date_fetch_end.str()))
+    while date_fetch_start <= date_max:
+        print('fetching between %s and %s' % (str(date_fetch_start),
+                                              str(date_fetch_end)))
 
         urlmap = er_get_urls(date_fetch_start, date_fetch_end)
         print('adding')
         add_urlmap(db, urlmap)
+        print('done')
         date_fetch_start = date_fetch_end
         date_fetch_end = date_fetch_end + batch_interval
 
 
+def run_service(db):
+    #urlmap = er_get_new()
+    pass
+
+
 def main():
 
-    try:
-        db = db_open()
-    except:
-        db_init()
-
     db = db_open()
+
+    if db is None:
+        db_init()
+        db = db_open()
+
+    if db is None:
+        print("Failed to open db")
+        return 0
+
+    run_service(db)
+
+    db.close()
+
 
 if __name__ == '__main__':
     main()
